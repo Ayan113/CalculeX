0a37b869ae09754cea34f631616e2c354bd40b96
Ayan_Chatterjee113 <ayanchat113@gmail.com>
2024-09-02 19:00:56 +0530

    chore:Add calculator app HTML,CSS and JavaScript files

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 const display=document.getElementById("display");
 function appendToDisplay(input){
-    display.value += input; // we are entering the input inside the display box on the calculator by display.value+=input
-
+    display.value+=input;
 }
 function clearDisplay(){
-    display.value=""; // we are giving an empty string to clear the display here , so the empty string works by clearing the display ! 
-
+    display.value="";
 }
 function calculate(){
     try{
-        display.value=eval(display.value); 
+        display.value=eval(display.value);
     }
     catch(error){
-        display.value="error !";
-
+        display.value="error";
     }
-    // eval() function is used to evaluate the expression inside the display box and then display it for this we are using here display.value=eval(display.value);
-
 }
